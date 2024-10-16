d1a49fc4dcd51b95d77202f552371d90506c267b
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2024-10-16 03:32:25 -0400

    Prevent sending display entities to client versions that don't support them

--- src/main/java/me/yirf/judge/events/OnSneak.java
+++ src/main/java/me/yirf/judge/events/OnSneak.java
@@ -1,5 +1,6 @@
 package me.yirf.judge.events;
 
+import com.viaversion.viaversion.api.Via;
 import me.yirf.judge.Judge;
 import me.yirf.judge.config.Config;
 import me.yirf.judge.group.Group;
@@ -25,6 +26,13 @@ public class OnSneak implements Listener {
 
         if(!Config.getBoolean("allow-all-worlds")) {
             if (!Judge.allowedWorlds.contains(p.getWorld())) {
+                return;
+            }
+        }
+
+        //Prevent displaying to the player if they are on a version without display entities (under 1.19.4) if ViaVersion is installed.
+        if(Bukkit.getServer().getPluginManager().isPluginEnabled("ViaVersion")) {
+            if(Via.getAPI().getPlayerVersion(p.getUniqueId()) < 762) {
                 return;
             }
         }
